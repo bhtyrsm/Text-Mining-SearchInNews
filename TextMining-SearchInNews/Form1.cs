691e786cdfe027573b995bad09b6f6fef8880d1c
Bahtiyar <bahtiyarsime@gmail.com>
2020-12-27 20:53:12 +0300

    Get All News
    Get All News

--- TextMining-SearchInNews/Form1.cs
+++ TextMining-SearchInNews/Form1.cs
@@ -38,10 +38,20 @@ namespace TextMining_SearchInNews
         {
             News news = new News();
             news.initializeLinks();
+
+            List<NewsDetail> totalNews = new List<NewsDetail>();
             if (news.NewsList != null && news.NewsList.Count() > 0)
             {
-                gridViewNews.DataSource = news.NewsList;
+              foreach(var item in news.NewsList)
+                {
+                    var response = GetNews(item.Source, item.Link);
+                    totalNews.AddRange(response);
+                }
             }
+
+            gridViewNews.DataSource = totalNews;
+            gridViewNews.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            gridViewNews.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
         }
 
         private void btnSeacrh_Click(object sender, EventArgs e)
@@ -82,96 +92,83 @@ namespace TextMining_SearchInNews
         /// 
         /// </summary>
         /// <returns></returns>
-        //public static List<DataRow> GetNews(string title,string link)
-        //{
-        //    try
-        //    {
+        public static List<NewsDetail> GetNews(string source, string link)
+        {
+            try
+            {
+                bool in_item = false;
+                string[] arr = { "", "", "" };
+                List<NewsDetail> newsDetails = new List<NewsDetail>();
 
-        //        bool in_item = false;
-        //        DataTable dt = new DataTable();
+                WebClient client = new WebClient();
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
-        //        string[] arr = { "", "", "" };
+                Uri address = new Uri(link);
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
-        //        DataRow dr;
-        //        dt.Columns.Add("id");
-        //        dt.Columns.Add("title");
-        //        dt.Columns.Add("desc");
+                using (Stream data = client.OpenRead(link))
+                {
+                    using (StreamReader stream = new StreamReader(data))
+                    {
+                        XmlTextReader reader = null;
+                        reader = new XmlTextReader(stream);
+                        while (reader.Read())
+                        {
+                            switch (reader.NodeType)
+                            {
+                                case XmlNodeType.Element: // The node is an Element.
+                                    if (reader.Name == "item")
+                                    {
+                                        in_item = true;
+                                    }
+                                    if ((reader.Name == "title") && (in_item))
+                                    {
+                                        arr[1] = reader.ReadString();
+                                    }
+                                    if ((reader.Name == "guid") && (in_item))
+                                    {
+                                        arr[0] = reader.ReadString();
 
-        //        WebClient client = new WebClient();
-        //        client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                                    }
+                                    if ((reader.Name == "description") && (in_item))
+                                    {
+                                        arr[2] = reader.ReadString();
 
-        //        Uri address = new Uri(link);
-        //        ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-        //        ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
-        //        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                                        arr[2] = ExtractText(arr[2]);
+                                        arr[2] = RemoveObsolete(arr[2]);
 
-        //        using (Stream data = client.OpenRead(link))
-        //        {
-        //            using (StreamReader stream = new StreamReader(data))
-        //            {
-        //                XmlTextReader reader = null;
-        //                reader = new XmlTextReader(stream);
-        //                while (reader.Read())
-        //                {
-        //                    switch (reader.NodeType)
-        //                    {
-        //                        case XmlNodeType.Element: // The node is an Element.
-        //                            if (reader.Name == "item")
-        //                            {
-        //                                in_item = true;
-        //                            }
-        //                            if ((reader.Name == "title") && (in_item))
-        //                            {
-        //                                arr[1] = reader.ReadString();
-        //                            }
-        //                            if ((reader.Name == "guid") && (in_item))
-        //                            {
-        //                                arr[0] = reader.ReadString();
+                                        NewsDetail newsDetail = new NewsDetail();
+                                        newsDetail.Source = source;
+                                        newsDetail.Title = arr[1];
+                                        newsDetail.Desciption= arr[2];
+                                        newsDetails.Add(newsDetail);
+                                    }
 
-        //                            }
-        //                            if ((reader.Name == "description") && (in_item))
-        //                            {
-        //                                arr[2] = reader.ReadString();
+                                    break;
+                                case XmlNodeType.Text: 
+                                    break;
+                                case XmlNodeType.EndElement: 
+                                    if (reader.Name == "item")
+                                    {
+                                        in_item = false;
+                                    }
+                                    break;
+                            }
 
-        //                                dr = dt.NewRow();
-        //                                dr["id"] = arr[0];
-        //                                dr["title"] = arr[1];
-        //                                // prepare the text
-        //                                arr[2] = ExtractText(arr[2]);
-        //                                arr[2] = RemoveObsolete(arr[2]);
+                        }
 
-        //                                dr["desc"] = arr[2];
-        //                                dt.Rows.Add(dr);
-        //                            }
-
-        //                            break;
-        //                        case XmlNodeType.Text: //Display the text in each element.
-        //                            // MessageBox.Show(reader.Value);
-        //                            break;
-        //                        case XmlNodeType.EndElement: //Display end of element.
-        //                            // MessageBox.Show("</" + reader.Name);
-        //                            if (reader.Name == "item")
-        //                            {
-        //                                in_item = false;
-        //                            }
-        //                            break;
-        //                    }
-
-        //                }
-
-
-        //                // string s = reader.ReadToEnd();
-        //                // ds.ReadXml(reader);
-        //                return dt;
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //        return null;
-        //    }
-        //}
+                        return newsDetails;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
 
         /// <summary>
         /// 
@@ -187,6 +184,7 @@ namespace TextMining_SearchInNews
             str = str.Replace("&#39;", "");
             return str;
         }
+
 
     }
 }
--- TextMining-SearchInNews/News.cs
+++ TextMining-SearchInNews/News.cs
@@ -16,32 +16,33 @@ namespace TextMining_SearchInNews
             this.NewsList = new List<News>();
 
         }
-        public string Title { get; set; }
+        public string Source { get; set; }
         public string Link { get; set; }
 
         public List<News> NewsList { get; set; }
 
         public void initializeLinks()
         {
-            this.NewsList.Add(new News() { Title = "Anadolu Ajansı", Link = "https://www.aa.com.tr/tr/rss/default?cat=guncel" });
-            this.NewsList.Add(new News() { Title = "Google Haberler", Link = "http://news.google.com/?output=rss" });
-            this.NewsList.Add(new News() { Title = "Dunya.Com", Link = "https://www.dunya.com/rss" });
-            this.NewsList.Add(new News() { Title = "Cumhuriyet(Son Dakika)", Link = "https://www.cumhuriyet.com.tr/rss/son_dakika.xml" });
-            this.NewsList.Add(new News() { Title = "Milliyet Gündem", Link = "https://www.milliyet.com.tr/rss/rssnew/gundemrss.xml" });
-            this.NewsList.Add(new News() { Title = "CNBC", Link = "https://www.cnbc.com/id/100727362/device/rss/rss.html" });
-            this.NewsList.Add(new News() { Title = "CNN Türk", Link = "https://www.cnnturk.com/feed/rss/news" });
-            this.NewsList.Add(new News() { Title = "Trt Gündem", Link = "http://www.trt.net.tr/rss/gundem.rss" });
-            this.NewsList.Add(new News() { Title = "AliJezara", Link = "http://www.aljazeera.com.tr/rss.xml" });
-            this.NewsList.Add(new News() { Title = "BBC", Link = "https://feeds.bbci.co.uk/turkce/rss.xml" });
-            this.NewsList.Add(new News() { Title = "CNN Türk(Türkiye)", Link = "https://www.cnnturk.com/feed/rss/turkiye/news" });
-            this.NewsList.Add(new News() { Title = "TRT Türkiye", Link = "http://www.trt.net.tr/rss/turkiye.rss" });
-            this.NewsList.Add(new News() { Title = "TRT Dünya", Link = "http://www.trt.net.tr/rss/dunya.rss" });
+            this.NewsList.Add(new News() { Source = "Anadolu Ajansı", Link = "https://www.aa.com.tr/tr/rss/default?cat=guncel" });
+            this.NewsList.Add(new News() { Source = "Google Haberler", Link = "http://news.google.com/?output=rss" });
+            this.NewsList.Add(new News() { Source = "Dunya.Com", Link = "https://www.dunya.com/rss" });
+            this.NewsList.Add(new News() { Source = "Cumhuriyet(Son Dakika)", Link = "https://www.cumhuriyet.com.tr/rss/son_dakika.xml" });
+            this.NewsList.Add(new News() { Source = "Milliyet Gündem", Link = "https://www.milliyet.com.tr/rss/rssnew/gundemrss.xml" });
+            this.NewsList.Add(new News() { Source = "CNBC", Link = "https://www.cnbc.com/id/100727362/device/rss/rss.html" });
+            this.NewsList.Add(new News() { Source = "CNN Türk", Link = "https://www.cnnturk.com/feed/rss/news" });
+            this.NewsList.Add(new News() { Source = "Trt Gündem", Link = "http://www.trt.net.tr/rss/gundem.rss" });
+            this.NewsList.Add(new News() { Source = "AliJezara", Link = "http://www.aljazeera.com.tr/rss.xml" });
+            this.NewsList.Add(new News() { Source = "BBC", Link = "https://feeds.bbci.co.uk/turkce/rss.xml" });
+            this.NewsList.Add(new News() { Source = "CNN Türk(Türkiye)", Link = "https://www.cnnturk.com/feed/rss/turkiye/news" });
+            this.NewsList.Add(new News() { Source = "TRT Türkiye", Link = "http://www.trt.net.tr/rss/turkiye.rss" });
+            this.NewsList.Add(new News() { Source = "TRT Dünya", Link = "http://www.trt.net.tr/rss/dunya.rss" });
         }
     }
 
 
     public class NewsDetail
     {
+        public string Source { get; set; }
         public string Title { get; set; }
         public string Desciption { get; set; }
         public string NewsLink { get; set; }
